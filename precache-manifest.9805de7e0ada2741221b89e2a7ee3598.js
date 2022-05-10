df634cc77a915496f7b0eb52514a1bee7159227b
oussemadhouib <oussema.d@littlebigsmala.com>
2022-05-10 16:29:37 +0100

    Updates

---
+++ precache-manifest.9805de7e0ada2741221b89e2a7ee3598.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aaf60d158594c63855b0ef12a31d7d0",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "017cc160b63bb889fd68",
+    "url": "/my-portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "a7bff8284ab1eca960b2",
+    "url": "/my-portfolio/static/css/main.e3c6e229.chunk.css"
+  },
+  {
+    "revision": "017cc160b63bb889fd68",
+    "url": "/my-portfolio/static/js/2.c40a0b9c.chunk.js"
+  },
+  {
+    "revision": "e670feed429893bbdb8c9e1ae51e424c",
+    "url": "/my-portfolio/static/js/2.c40a0b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7bff8284ab1eca960b2",
+    "url": "/my-portfolio/static/js/main.e5757d20.chunk.js"
+  },
+  {
+    "revision": "1e5eabb91300b2757a88",
+    "url": "/my-portfolio/static/js/runtime-main.571a3e70.js"
+  },
+  {
+    "revision": "4ac0bc679a0efe6a0b90d5db27a755f8",
+    "url": "/my-portfolio/static/media/me.4ac0bc67.jpeg"
+  },
+  {
+    "revision": "1322fe05377dd9eadd0b80b9f0a37380",
+    "url": "/my-portfolio/static/media/smala.1322fe05.png"
+  }
+]);
