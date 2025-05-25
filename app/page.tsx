580c3f284cc985cca63a08fb05e081b7f1d2e664
oussema17h <oussema@relentlessco.co>
2025-05-25 15:48:21 +0200

    fix: account github

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import About from "@/components/about";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Contact from "@/components/contact";
-import Chatbot from "@/components/chatbot";
+// import Chatbot from "@/components/chatbot";
 
 export default function Home() {
   return (
